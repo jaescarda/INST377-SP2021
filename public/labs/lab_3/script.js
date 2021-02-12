d23b3480a4c563bc3cd666f608b1cfcf7c50a1fa
Julia Escarda <78128989+jaescarda@users.noreply.github.com>
2021-02-12 18:17:59 -0500

    pictures move right but disgustingly

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,22 @@
 /* Put your javascript in here */
+let position = 0;    
+const listOfImages = document.querySelector('ul');
+const listOfImageElems = document.querySelectorAll('li');
+const imageArray = Array.from(listOfImageElems);
+
+function movePictureRight() {
+    imageArray.forEach(element => {
+        position += 200;
+        element.style.left = position + 'px'
+        console.log(element);
+    })
+};
+
+function clickToMove() {
+    const rightArrow = document.querySelector('#right')
+    rightArrow.addEventListener('click', (event) => {
+        movePictureRight()
+    })
+}
+
+window.onload = clickToMove;
