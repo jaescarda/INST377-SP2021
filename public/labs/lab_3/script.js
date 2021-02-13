3b0a02a46478504bc5bdeaaddb32e54bd7ae51eb
Julia Escarda <78128989+jaescarda@users.noreply.github.com>
2021-02-12 19:27:50 -0500

    almost but not really

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,22 +1,39 @@
 /* Put your javascript in here */
-let position = 0;    
+let position = 0;
+let width = 130;
+let count = 3;
 const listOfImages = document.querySelector('ul');
 const listOfImageElems = document.querySelectorAll('li');
 const imageArray = Array.from(listOfImageElems);
 
 function movePictureRight() {
     imageArray.forEach(element => {
-        position += 200;
-        element.style.left = position + 'px'
+        position += 10;
+        element.style.left = position + 'px';
         console.log(element);
     })
 };
 
+function movePictureLeft() {
+    imageArray.forEach(element => {
+        position -= 10;
+        element.style.left = position + 'px';
+        console.log(element);
+    })
+};
+
+
 function clickToMove() {
-    const rightArrow = document.querySelector('#right')
+    const rightArrow = document.querySelector('#next')
+    const leftArrow = document.querySelector('#prev')
     rightArrow.addEventListener('click', (event) => {
-        movePictureRight()
+        movePictureRight();
+    })
+
+    leftArrow.addEventListener('click', (event) => {
+        movePictureLeft();
     })
 }
 
-window.onload = clickToMove;
+
+window.onload = clickToMove; 
